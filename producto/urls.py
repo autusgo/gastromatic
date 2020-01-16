2ab1abc395174bb460b0dc17448dbe452ec51e64
autusgo <autusgo@outlook.com>
2020-01-16 20:17:19 -0300

    Agregué poder crear productos, editarlos y seguridad para que nadie pueda hacerlo sin loguearse

--- producto/urls.py
+++ producto/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.product_list, name='product_list'),
     path('producto/<int:pk>/', views.producto_detail, name='producto_detail'),
+    path('producto/new/', views.producto_new, name='producto_new'),
+    path('producto/<int:pk>/edit/', views.producto_edit, name='producto_edit'),
 ]
--- producto/views.py
+++ producto/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Producto
+from .forms import ProductoForm
+from django.shortcuts import redirect
 
 # Create your views here.
 def product_list(request):
@@ -9,3 +11,30 @@ def product_list(request):
 def producto_detail(request, pk):
     producto = get_object_or_404(Producto, pk=pk)
     return render(request, 'producto/producto_detail.html', {'producto': producto})
+
+def producto_new(request):
+    if request.method == "POST":
+        form = ProductoForm(request.POST)
+        if form.is_valid():
+            producto = form.save(commit=False)
+            #post.author = request.user
+            #post.published_date = timezone.now()
+            producto.save()
+            return redirect('producto_detail', pk=producto.pk)
+    else:
+        form = ProductoForm()
+    return render(request, 'producto/producto_edit.html', {'form': form})
+
+def producto_edit(request, pk):
+    producto = get_object_or_404(Producto, pk=pk)
+    if request.method == "POST":
+        form = ProductoForm(request.POST, instance=producto)
+        if form.is_valid():
+            producto = form.save(commit=False)
+            #post.author = request.user
+            #post.published_date = timezone.now()
+            producto.save()
+            return redirect('producto_detail', pk=producto.pk)
+    else:
+        form = ProductoForm(instance=producto)
+    return render(request, 'producto/producto_edit.html', {'form': form})
