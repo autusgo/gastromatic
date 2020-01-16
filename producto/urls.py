730468facac76766d9449b5ccb55204925de6735
autusgo <autusgo@outlook.com>
2020-01-15 22:02:14 -0300

    Agregue el template de cada producto

--- producto/urls.py
+++ producto/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.product_list, name='product_list'),
+    path('producto/<int:pk>/', views.producto_detail, name='producto_detail'),
 ]
--- producto/views.py
+++ producto/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Producto
 
 # Create your views here.
 def product_list(request):
     productos = Producto.objects.all().order_by('nombre')
     return render(request, 'producto/product_list.html', {'productos': productos})
+
+def producto_detail(request, pk):
+    producto = get_object_or_404(Producto, pk=pk)
+    return render(request, 'producto/producto_detail.html', {'producto': producto})
