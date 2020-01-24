ae17c72833e94190e059590c63ce64755b80b146
autusgo <autusgo@outlook.com>
2020-01-23 22:23:32 -0300

    Agregué opciones de fecha a la factura

--- compras/models.py
+++ compras/models.py
@@ -6,6 +6,9 @@ from decimal import Decimal
 from localflavor.ar.forms import ARCUITField
 from phonenumber_field.modelfields import PhoneNumberField
 import datetime
+from model_utils import Choices
+from model_utils.fields import MonitorField
+from django.utils.translation import ugettext_lazy as _
 
 #PRODUCTOS
 class Producto(models.Model):
@@ -51,18 +54,30 @@ class Proveedor(models.Model):
 
 #FACTURAS
 class Factura(models.Model):
+
+    FACTURA_ESTADO = Choices(
+        ('nopago', _(u'NO PAGO')),
+        ('pago', _(u'PAGO')),
+        #('PAGO PARCIAL', _(u'pagoparcial'))
+    )
+
+    fecha = models.DateField(default=datetime.date.today)
     numero = models.CharField(max_length=10, default='1')
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     proveedor = models.ForeignKey(Proveedor, on_delete=models.CASCADE)
     producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
     cantidad = models.DecimalField(max_digits=5 , decimal_places=0)
     monto = models.DecimalField(max_digits=9 , decimal_places=2)
-    estado = models.CharField(max_length=50)
-    fecha = models.DateField(default=datetime.date.today)
-
+    #total = models.DecimalField(_(u'total'), max_digits=10, decimal_places=2, blank=True, default=0)
+    estado = models.CharField(_(u'estado'), choices=FACTURA_ESTADO, max_length=64, default=FACTURA_ESTADO.nopago)
+    fecha_de_pago = MonitorField(monitor='estado', when=[FACTURA_ESTADO.pago], verbose_name=_(u'Fecha de pago'), blank=True, null=True, default=None)
 
     class Meta:
         verbose_name_plural = "Facturas"
 
     def __str__(self):
         return '{} {} {}'.format(self.numero, self.monto, self.estado)
+
+    #@property
+    #def total(self):
+        #total = round(self.monto * self.cantidad, 2)
+        #return round(total, 2)
